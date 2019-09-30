8d3c0d5861b1f861c7b469208cb7a769e6514f65
RaghunandanKumar <naishadamraghu@gmail.com>
2019-09-30 11:26:25 -0500

    modified the app

--- calc.js
+++ calc.js
@@ -1,22 +1,17 @@
-function largest(x,y)
-{
-   if ((x>y) )
-   {
-       return document.querySelector('#firstString').value
-   }
-   else
-   {
-       return y
-   } 
+function largest(x, y) {
+    if (x > y) {
+        return document.querySelector('#firstString').value
+    }
+    else {
+        return document.querySelector('#secondString').value
+    }
 }
 
 document.ready
-document.querySelector('#addition').addEventListener('click', () => {   
-    const s = document.querySelector('#guest').value   
-    const i =document.querySelector('#firstString').value.length   
-    const j =document.querySelector('#secondString').value.length
-     
-   
-    const ans =    s +', your largest string is '+largest(i,j)
+document.querySelector('#addition').addEventListener('click', () => {
+    const s = document.querySelector('#guest').value
+    const i = document.querySelector('#firstString').value.length
+    const j = document.querySelector('#secondString').value.length
+    const ans = s + ', your largest word is ' + largest(i, j)
     document.getElementById('result').innerHTML = ans
 })
