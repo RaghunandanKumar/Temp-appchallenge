b0d6f44969ac0b5f9a3e20e1254bb7888d9bb624
RaghunandanKumar <naishadamraghu@gmail.com>
2019-09-30 11:14:46 -0500

    added js script

--- calc.js
+++ calc.js
@@ -1,58 +1,22 @@
-function add(x, y,z) 
+function largest(x,y)
 {
-     return x + y+z 
-}
-function mul(x,y,z)
-{
-    return x * y *z
+   if ((x>y) )
+   {
+       return document.querySelector('#firstString').value
+   }
+   else
+   {
+       return y
+   } 
 }
 
-function average(x,y,z)
-{
-    return (x+y+z)/3
-}
-
-function smallest(x,y,z)
-{
-    if((x<y)&&(x<z))
-    {
-        return x
-    }
-    else if((y<z)&&(y<x))
-    {
-        return y
-    }
-else {
-    return z
-}
-}
 document.ready
 document.querySelector('#addition').addEventListener('click', () => {   
     const s = document.querySelector('#guest').value   
-    const i = parseInt(document.querySelector('#firstNumber').value)   
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const k = parseInt(document.querySelector('#thirdnumber').value) 
-    addcount=0
-    if(localStorage.getItem("addcount")!=null)
-        addcount=localStorage.getItem("addcount");
-    addcount++
-    localStorage.setItem("addcount",addcount);
-    const ans =    s +', your sum is '+add(i,j,k)+' This has been '+ addcount
-    document.getElementById('result').innerHTML = ans
-})
-document.querySelector('#average').addEventListener('click', () => {   
-    const s = document.querySelector('#guest').value.length  
-    const i = parseInt(document.querySelector('#firstNumber').value)   
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const k = parseInt(document.querySelector('#thirdnumber').value) 
-    const ans = `   ${s} , your avrage is ${average(i,j,k)}.`
-    document.getElementById('result').innerHTML = ans
-})
-document.querySelector('#multiplication').addEventListener('click', () => {   
-    const s = document.querySelector('#guest').value   
-    const i = parseInt(document.querySelector('#firstNumber').value)   
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const k = parseInt(document.querySelector('#thirdnumber').value) 
-    const ans = `   ${s} , your multiplication is ${add(i,j,k)}.`
+    const i =document.querySelector('#firstString').value.length   
+    const j =document.querySelector('#secondString').value.length
+     
+   
+    const ans =    s +', your largest string is '+largest(i,j)
     document.getElementById('result').innerHTML = ans
 })
