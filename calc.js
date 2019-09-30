83e099c72af5f0d79033136ebd0149adc17fb2c3
RaghunandanKumar <naishadamraghu@gmail.com>
2019-09-30 11:46:59 -0500

    added into repo

--- calc.js
+++ calc.js
@@ -12,6 +12,17 @@ document.querySelector('#addition').addEventListener('click', () => {
     const s = document.querySelector('#guest').value
     const i = document.querySelector('#firstString').value.length
     const j = document.querySelector('#secondString').value.length
+    if(i==j)
+    {
+        const ans = "Enter Valid input"
+        document.getElementById('result').innerHTML = ans
+        }
+        else
+        {
     const ans = s + ', your largest word is ' + largest(i, j)
     document.getElementById('result').innerHTML = ans
+}
+
+
+    
 })
